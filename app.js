3e6bdf0fcbbb99daa1c8e5a9da906bd9178ee7f8
Brandon Jang <brandonjang90@gmail.com>
2023-09-12 12:47:33 -0700

    finished assignment

--- app.js
+++ app.js
@@ -21,13 +21,24 @@ const footer = document.getElementsByClassName('footer');
 footer.classList.add("main");
 
 // 7. Remove the class main on the div with a class of footer.
+footer.classList.remove("main");
 
 // 8. Create a new li element.
+const li = document.createElement("li");
 
 // 9. Give the li the text “four”.
+li.innerText = "four"
 
 // 10. Append the li to the ul element.
+ul.appendChild(li);
 
 // 11. Loop over all of the lis inside the ol tag and give them a background color of “green”.
-
+function giveColor(){
+    const list = document.querySelectorAll('ol');
+}
+for (let li of list) {
+    li.style.backgroundColot = "green"
+}
 // 12. Remove the div with a class of footer
+let footer = document.querySelector(".footer");
+footer.remote();
